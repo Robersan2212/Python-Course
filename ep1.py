1328cf07ac8eaa6d4c03b27825855ae864bb87bf
Roberto <san20086@byui.edu>
2025-02-26 14:32:50 -0700

    finish episode 1 and 2.

---
+++ ep1.py
@@ -0,0 +1,14 @@
+#print statemet
+#print("Hello World!!")
+
+#String is a seuence of character or just text. The way to know is if it has "" or ''
+
+#print("""I am Ironman.
+    #No, I am Tony Stark.
+    #No, I am Poppy.""")
+
+print("I am Iron Man. \n" + "No, I am Tony Stark. \n" + "No, I am Poppy. ")
+
+
+
+print("I am Poppy\n" * 100)
---
+++ ep2.py
@@ -0,0 +1,25 @@
+#Variables in python
+
+#Let's start a coffee shop together!! We're going to buidl a cofee shop using some new Python programming concepts!!
+
+#Lets's build a robot barista
+
+print("Hello welcome to Roberto's cofee shop!!!")
+
+costumer_name = input("What is your name? \n")
+
+print(f"Hello, {costumer_name}, I will be your barista, thank you for coming in today. \n\n\n")
+
+menu = "Black Coffe, Espresso, Latte, Cappucino"
+
+print(f" {costumer_name}, here is our menu:\n {menu}")
+
+print("What would you like form our menu? ")
+
+order = input()
+
+print(f"It is going to be {order}, and it will be ready in 5 minute.")
+
+
+
+
