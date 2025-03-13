9d2d1318302f120dc1aa9d65d37d0f1bab41e656
Roberto <san20086@byui.edu>
2025-03-13 14:34:39 -0600

    finish episode 2 and 3.

--- ep2.py
+++ ep2.py
@@ -4,21 +4,28 @@
 
 #Lets's build a robot barista
 
+
 print("Hello welcome to Roberto's cofee shop!!!")
 
 costumer_name = input("What is your name? \n")
 
 print(f"Hello, {costumer_name}, I will be your barista, thank you for coming in today. \n\n\n")
 
-menu = "Black Coffe, Espresso, Latte, Cappucino"
+menu = "Black Coffe , Espresso, Latte, Cappucino"
 
 print(f" {costumer_name}, here is our menu:\n {menu}")
 
-print("What would you like form our menu? ")
+print("What would you like from our menu? ")
 
 order = input()
 
-print(f"It is going to be {order}, and it will be ready in 5 minute.")
+price = 8
+
+quantity = int(input("How many coffees would you like?\n"))
+
+total = price * quantity
+
+print(f"It is going to be {quantity} {order}. Your total is: ${total}, and it will be ready in 5 minute.")
 
 
 
---
+++ ep3.py
@@ -0,0 +1,11 @@
+name = "Roberto"
+age = 23
+actual_age = 23.45
+math = 5 ** 7 + 6 / 9 * 6 - 4
+result = age + actual_age + math
+
+print(result)
+
+
+
+
